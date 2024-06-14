fe47dc29074da34a695b4166a072b7732383fa2d
Hecate <hecate0821@outlook.com>
2024-06-14 23:08:17 +0800

    feature: remove lock, switch to fixed thread, add command args for test

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import test_classifier as cl
 from huggingface_hub import snapshot_download
 import threading
@@ -13,7 +14,7 @@ access_token = "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"
 max_disk_usage = 100 * 1024 * 1024 * 1024 * 10 # 1000GB
 
 
-allow_patterns_list = ['CC-MAIN-2013-20/000_00000.parquet', 'CC-MAIN-2013-20/000_00001.parquet', 'CC-MAIN-2013-20/000_00002.parquet'
+default_patterns_list = ['CC-MAIN-2013-20/000_00000.parquet', 'CC-MAIN-2013-20/000_00001.parquet', 'CC-MAIN-2013-20/000_00002.parquet'
                        'CC-MAIN-2013-48', 'CC-MAIN-2014-10', 'CC-MAIN-2014-15', 'CC-MAIN-2014-23', 'CC-MAIN-2014-35', 'CC-MAIN-2014-41', 'CC-MAIN-2014-42', 'CC-MAIN-2014-49', 'CC-MAIN-2014-52', 'CC-MAIN-2015-06', 'CC-MAIN-2015-11', 'CC-MAIN-2015-14', 'CC-MAIN-2015-18', 'CC-MAIN-2015-22', 'CC-MAIN-2015-27', 'CC-MAIN-2015-32', 'CC-MAIN-2015-35', 'CC-MAIN-2015-40', 'CC-MAIN-2015-48', 'CC-MAIN-2016-07', 'CC-MAIN-2016-18', 'CC-MAIN-2016-22', 'CC-MAIN-2016-26', 'CC-MAIN-2016-30', 'CC-MAIN-2016-36', 'CC-MAIN-2016-40', 'CC-MAIN-2016-44', 'CC-MAIN-2016-50', 'CC-MAIN-2017-04', 'CC-MAIN-2017-09', 'CC-MAIN-2017-13', 'CC-MAIN-2017-17', 'CC-MAIN-2017-22', 'CC-MAIN-2017-26', 'CC-MAIN-2017-30', 'CC-MAIN-2017-34', 'CC-MAIN-2017-39', 'CC-MAIN-2017-43', 'CC-MAIN-2017-47', 'CC-MAIN-2017-51', 'CC-MAIN-2018-05', 'CC-MAIN-2018-09', 'CC-MAIN-2018-13', 'CC-MAIN-2018-17', 'CC-MAIN-2018-22', 'CC-MAIN-2018-26', 'CC-MAIN-2018-30', 'CC-MAIN-2018-34', 'CC-MAIN-2018-39', 'CC-MAIN-2018-43', 'CC-MAIN-2018-47', 'CC-MAIN-2018-51', 'CC-MAIN-2019-04', 'CC-MAIN-2019-09', 'CC-MAIN-2019-13', 'CC-MAIN-2019-18', 'CC-MAIN-2019-22', 'CC-MAIN-2019-26', 'CC-MAIN-2019-30', 'CC-MAIN-2019-35', 'CC-MAIN-2019-39', 'CC-MAIN-2019-43', 'CC-MAIN-2019-47', 'CC-MAIN-2019-51', 'CC-MAIN-2020-05', 'CC-MAIN-2020-10', 'CC-MAIN-2020-16', 'CC-MAIN-2020-24', 'CC-MAIN-2020-29', 'CC-MAIN-2020-34', 'CC-MAIN-2020-40', 'CC-MAIN-2020-45', 'CC-MAIN-2020-50', 'CC-MAIN-2021-04', 'CC-MAIN-2021-10', 'CC-MAIN-2021-17', 'CC-MAIN-2021-21', 'CC-MAIN-2021-25', 'CC-MAIN-2021-31', 'CC-MAIN-2021-39', 'CC-MAIN-2021-43', 'CC-MAIN-2021-49', 'CC-MAIN-2022-05', 'CC-MAIN-2022-21', 'CC-MAIN-2022-27', 'CC-MAIN-2022-33', 'CC-MAIN-2022-40', 'CC-MAIN-2022-49', 'CC-MAIN-2023-06', 'CC-MAIN-2023-14', 'CC-MAIN-2023-23', 'CC-MAIN-2023-40', 'CC-MAIN-2023-50', 'CC-MAIN-2024-10', 'CC-MAIN-2024-18']
 
 process_lock = threading.Lock()  # Global lock for process
@@ -23,7 +24,6 @@ def get_disk_usage():
     total, used, free = shutil.disk_usage(local_dir)
     return used
 
-
 def download_dataset(allow_patterns):
     filepath = snapshot_download(
         download_hub,
@@ -32,10 +32,8 @@ def download_dataset(allow_patterns):
         allow_patterns="data/" + allow_patterns)
     return filepath
 
-
 def upload_dataset(dataset):
     dataset.push_to_hub(upload_hub, config_name='test', private=False, max_shard_size="4096MB", token=access_token)
-
 
 def delete_dataset(filepath):
     if os.path.exists(filepath):
@@ -44,11 +42,10 @@ def delete_dataset(filepath):
         else:
             shutil.rmtree(filepath)
 
-
 class DatasetHandler:
-    def __init__(self, patterns_list):
+    def __init__(self, patterns_list, num_threads):
         self.patterns_list = patterns_list
-        self.current_index = 0
+        self.num_threads = num_threads
         self.lock = threading.Lock()
 
     def process_and_download(self, pattern):
@@ -57,16 +54,14 @@ class DatasetHandler:
 
         dataset = cl.my_load_dataset(filepath)
 
-        process_lock.acquire()
         print("\nProcessing pattern: " + pattern + "\n")
         cl.process_dataset(dataset)
-        process_lock.release()
 
         upload_dataset(dataset)
         delete_dataset(filepath)
 
     def run(self):
-        with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=self.num_threads) as executor:
             futures = []
             for pattern in self.patterns_list:
                 futures.append(executor.submit(self.process_and_download, pattern))
@@ -77,6 +72,26 @@ class DatasetHandler:
                 except Exception as e:
                     print(f"Exception occurred: {e}")
 
+def parse_args():
+    parser = argparse.ArgumentParser(description="Dataset handling script.")
+    parser.add_argument('-t', '--threads', type=int, default=3, help='Number of threads in the thread pool')
+    parser.add_argument('-j', '--json', type=str, help='Path to JSON file with allow patterns list')
+    return parser.parse_args()
 
-handler = DatasetHandler(allow_patterns_list)
-handler.run()
+def main():
+    args = parse_args()
+
+    if args.json:
+        with open(args.json, 'r') as f:
+            data = json.load(f)
+            allow_patterns_list = data.get("patterns", default_patterns_list)
+    else:
+        allow_patterns_list = default_patterns_list
+
+    print("pattern list: \n" + str(allow_patterns_list))
+
+    handler = DatasetHandler(allow_patterns_list, args.threads)
+    handler.run()
+
+if __name__ == "__main__":
+    main()
