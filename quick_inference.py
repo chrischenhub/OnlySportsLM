5692466d76754fb191c41d07034a4b6857f1d5fd
chrischenhub <zc2404@nyu.edu>
2024-06-17 14:24:40 -0400

    Update quick_inference.py

--- quick_inference.py
+++ quick_inference.py
@@ -11,7 +11,7 @@ tokenizer = AutoTokenizer.from_pretrained('Chrisneverdie/sports-text-classifier'
 model = AutoModelForSequenceClassification.from_pretrained('Chrisneverdie/sports-text-classifier', torch_dtype=torch.bfloat16)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
-dataset = load_dataset('Chrisneverdie/sports-annotation-test',data_files={'train': 'sports_annotation_test.parquet'})
+dataset = load_dataset('Chrisneverdie/sports-annotation-test',data_files={'train': 'nonsport.parquet'})
 
 def compute_scores(batch):
     inputs = tokenizer(batch['text'], return_tensors="pt", padding="longest", truncation=True).to(device)
@@ -19,10 +19,7 @@ def compute_scores(batch):
         outputs = model(**inputs)
         logits = outputs.logits.squeeze(-1).float().cpu().numpy()
 
-    #predicted_class_id = logits.argmax().item()
     batch["score"] =  logits.tolist()
-   # batch['label'] = logits.argmax().tolist()
-    #batch["int_score"] = [int(round(max(0, min(score, 5)))) for score in logits]
     return batch
 
 def add_prefix(example):
@@ -31,4 +28,5 @@ def add_prefix(example):
 
 dataset = dataset.map(compute_scores, batched=True, batch_size=512)
 dataset = dataset.map(add_prefix)
-dataset.push_to_hub('Chrisneverdie/sports-annotation-outcome', config_name='test', private=False, max_shard_size="4096MB",token=access_token)
+dataset = dataset.filter(lambda example: example["ypred"]==1)
+dataset.push_to_hub('Chrisneverdie/sports-annotation-outcome', config_name='nonsport_outcome', private=False, max_shard_size="4096MB",token=access_token)
