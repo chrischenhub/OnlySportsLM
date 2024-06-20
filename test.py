12a491c0c6ec516410764c181e3e32d3c22f7eb9
Hecate <hecate0821@outlook.com>
2024-06-20 22:34:03 +0800

    fix: add log delete fixed

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
 from main import local_download_dir, allow_patterns_prefix
 import os
+from datasets import load_dataset
+import shutil
+access_token = "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"
 
-pattern = "pattern"
-file_path = "D:/file/to/pathfile"
 
-print(os.path.basename(file_path))
+def delete_dataset(filepath):
+    if os.path.exists(filepath):
+        if os.path.isfile(filepath):
+            os.remove(filepath)
+        else:
+            shutil.rmtree(filepath)
+
+delete_dataset("downloads/test/data/pattern/test2")
