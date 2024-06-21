5aa3c7b376d31219d669f6cce5883c90ae7f1652
chrischenhub <zc2404@nyu.edu>
2024-06-21 05:26:32 -0400

    Create dataloader_test.py

---
+++ dataloader_test.py
@@ -0,0 +1,21 @@
+import argparse
+import threading
+import json
+from main import allow_patterns_prefix, default_patterns_list, download_dataset, upload_dataset, delete_dataset, local_download_dir
+from DataGenerator import keywords
+import os
+import concurrent.futures
+from datasets import load_dataset, disable_caching
+from filelock import FileLock
+
+#disable_caching()
+
+cache_dir = "/root/.cache/huggingface"
+
+dataset = load_dataset("HuggingFaceFW/fineweb", "CC-MAIN-2019-04",
+                    split="train", cache_dir="/cache/", num_proc=12)
+
+
+dataset = dataset.select_columns(['text', 'url', 'dump', 'token_count'])
+dataset = dataset.filter(lambda example: any(keyword in example["url"] for keyword in keywords))
+upload_dataset(dataset, "CC-MAIN-2019-04")
