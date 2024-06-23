f071032ec3482cb4a0ec3fbf0ad257d627a79604
chrischenhub <zc2404@nyu.edu>
2024-06-23 15:36:09 -0400

    Update new_filter.py

--- new_filter.py
+++ new_filter.py
@@ -1,4 +1,5 @@
 import pandas as pd
+access_token = "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"
 
 
 # keywords = [
@@ -28,15 +29,16 @@ keywords = [
     'nytimes/athletic', 'apnews.com', 'goal',
 ]
 
-def filter_urls(df):
-    pattern = '|'.join(keywords)  # Create a regex pattern from the keywords
-    df_filtered = df[df['url'].str.contains(pattern, case=False, na=False)]  # Use vectorized string operations
-    return df_filtered
+# def filter_urls(df):
+#     pattern = '|'.join(keywords)  # Create a regex pattern from the keywords
+#     df_filtered = df[df['url'].str.contains(pattern, case=False, na=False)]  # Use vectorized string operations
+#     return df_filtered
 
 import os
 from pyarrow import parquet as pq
 from pyarrow import Table
 import gc
+from datasets import load_dataset, Dataset, DatasetDict
 
 input_directory = './downloads/test/data/CC-MAIN-2017-17/'
 output_directory = './downloads/test/data/CC-MAIN-2017-17-filtered/'
@@ -44,37 +46,57 @@ output_directory = './downloads/test/data/CC-MAIN-2017-17-filtered/'
 if not os.path.exists(output_directory):
     os.makedirs(output_directory)
 
-for filename in os.listdir(input_directory):
-    if filename.endswith(".parquet"):
-        filepath = os.path.join(input_directory, filename)
-        df = pd.read_parquet(filepath)
-        filtered_df = filter_urls(df)
-        output_path = os.path.join(output_directory, f"filtered_{filename}")
-        filtered_df.to_parquet(output_path)
-        print(f'{filename} filtered')
-        del df, filtered_df
-        os.remove(filepath)
-        gc.collect()
-        
+# for filename in os.listdir(input_directory):
+#     if filename.endswith(".parquet"):
+#         filepath = os.path.join(input_directory, filename)
+#         df = pd.read_parquet(filepath)
+#         filtered_df = filter_urls(df)
+#         output_path = os.path.join(output_directory, f"filtered_{filename}")
+#         filtered_df.to_parquet(output_path)
+#         print(f'{filename} filtered')
+#         del df, filtered_df
+#         os.remove(filepath)
+#         gc.collect()
 
+def filter_dataset(dataset, keywords):
+    return dataset.filter(lambda example: any(keyword in example["url"] for keyword in keywords))
 
-import pyarrow as pa
-
-def concatenate_parquet_files(input_directory, output_file):
-    all_dfs = []
+def process_and_filter_files():
+    all_filtered_datasets = []
     for filename in os.listdir(input_directory):
         if filename.endswith(".parquet"):
             filepath = os.path.join(input_directory, filename)
-            table = pq.read_table(filepath)
-            all_dfs.append(table)
-           # os.remove(filepath)  # Delete the filtered file after reading
-            del table
+            dataset = load_dataset('parquet', data_files=filepath, split='train')
+            filtered_dataset = filter_dataset(dataset, keywords)
+            output_path = os.path.join(output_directory, f"filtered_{filename}")
+            filtered_dataset.to_parquet(output_path)
+            all_filtered_datasets.append(filtered_dataset)
+            os.remove(filepath) 
+            gc.collect()
+    concatenated_dataset = Dataset.concatenate(*all_filtered_datasets)
+    #concatenated_dataset.to_parquet(final_output_file)
+    #concatenated_dataset.push_to_hub(repo_id='repo_id', dataset_name=dataset_name)
+    concatenated_dataset.push_to_hub("Chrisneverdie/OnlySports", data_dir='C-MAIN-2017-17_fixed', private=False, max_shard_size="4096MB", token=access_token)
 
-    concatenated_table = pa.concat_tables(all_dfs)
-    pq.write_table(concatenated_table, output_file)
-    del all_dfs, concatenated_table
+process_and_filter_files()
 
-print('Concatenating filtered files...')
-output_file = './downloads/outcome/CC-MAIN-2017-17'
-concatenate_parquet_files(output_directory, output_file)
+# import pyarrow as pa
+
+# def concatenate_parquet_files(input_directory, output_file):
+#     all_dfs = []
+#     for filename in os.listdir(input_directory):
+#         if filename.endswith(".parquet"):
+#             filepath = os.path.join(input_directory, filename)
+#             table = pq.read_table(filepath)
+#             all_dfs.append(table)
+#            # os.remove(filepath)  # Delete the filtered file after reading
+#             del table
+
+#     concatenated_table = pa.concat_tables(all_dfs)
+#     pq.write_table(concatenated_table, output_file)
+#     del all_dfs, concatenated_table
+
+# print('Concatenating filtered files...')
+# output_file = './downloads/outcome/CC-MAIN-2017-17'
+# concatenate_parquet_files(output_directory, output_file)
 
