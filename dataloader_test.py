fa04a82cfeb7d7554543c65fa1f8609626a6ca3e
chrischenhub <zc2404@nyu.edu>
2024-06-28 15:32:40 -0400

    update

--- dataloader_test.py
+++ dataloader_test.py
@@ -11,15 +11,46 @@ from datasets import disable_caching
 disable_caching()
 
 access_token = "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"
+cache_dir = "/root/.cache/huggingface"
 
 #disable_caching()
-RETRY_LIMIT = 8  # 设置重试次数
+RETRY_LIMIT = 5 # 设置重试次数
+
+def delete_files(file_path):
+    for root, dirs, files in os.walk(file_path, topdown=False):
+        for name in files:
+            file_path = os.path.join(root, name)
+            os.remove(file_path)
+        for name in dirs:
+            dir_path = os.path.join(root, name)
+            os.rmdir(dir_path)
+
+
 
 def process_data(name):
-    dataset = load_dataset("HuggingFaceFW/fineweb", name,
-                        split="train", num_proc=8)
 
-    dataset = dataset.select_columns(['text', 'url', 'token_count'])
+    retry_count = 0
+    while retry_count < RETRY_LIMIT:
+        try:
+            dataset = load_dataset("HuggingFaceFW/fineweb", name,
+                        split="train", num_proc=8)
+            dataset = dataset.select_columns(['text', 'url', 'token_count'])
+
+            break
+        except Exception as e:
+            retry_count += 1
+  
+
+            if retry_count >= RETRY_LIMIT:
+                error_message = f"Failed to upload dataset after {RETRY_LIMIT} retries. Error: {str(e)}"
+                with open("upload_error.txt", "a") as file:
+                    file.write(error_message + "\n")
+                print(error_message)
+
+    # dataset = load_dataset("HuggingFaceFW/fineweb", name,
+    #                     split="train", num_proc=8)
+
+    # dataset = dataset.select_columns(['text', 'url', 'token_count'])
     print('Dataset loaded, filtering...')
     dataset = dataset.filter(lambda example: any(keyword in example["url"] for keyword in keywords),num_proc=8)
     print('Dataset filtered, uploading...')
@@ -43,8 +74,24 @@ def process_data(name):
     print('done')
 
 
+def loop(patten):
+    for i in patten:
+        process_data(i)
+        delete_files(cache_dir)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process parquet files to filter sports URLs.")
-    parser.add_argument("-n", "--name", type=str, help="Target pattern in the hub")
+    parser.add_argument('-j', '--json', type=str, help='Path to JSON file with allow patterns list')
+    parser.add_argument('-n', '--name', type=str, help='Path to JSON file with allow patterns list')
+
+
+
     args = parser.parse_args()
-    process_data(args.name)
+    if args.json:
+        with open(args.json, 'r') as f:
+            data = json.load(f)
+            allow_patterns_list = data.get("patterns", default_patterns_list)
+    else:
+        allow_patterns_list = [args.name]
+
+    process_data(allow_patterns_list)
